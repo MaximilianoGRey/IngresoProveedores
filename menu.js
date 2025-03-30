923d10649ae511e8629fddc6c58c2fdc2720bf52
Maximiliano Rey <maximilianogrey@hotmail.com>
2025-03-30 09:48:36 -0300

    cargando

---
+++ menu.js
@@ -0,0 +1,8 @@
+const menu = document.querySelector('.menu');
+
+const menuDesplegable = document.querySelector('.menuDesplegable');
+
+menu.addEventListener('click',()=>{
+    menu.classList.toggle('active');
+    menuDesplegable.classList.toggle("click");
+})
